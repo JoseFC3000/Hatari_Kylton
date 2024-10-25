e11cc121a310e4c689e1105d5c38c26f175ea58f
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-10-25 04:47:57 +0100

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -609,26 +609,26 @@ static void Screen_SetResolution(void)
  		}
 		else    /* else use 640x400, also for med-rez */
  		{
- 			Width = 640;
- 			Height = 400;
-			nZoom = 2;
+ 			Width = 320;
+ 			Height = 200;
+			nZoom = 1;
  		}
 
 		/* Statusbar height for doubled screen size */
-		SBarHeight = Statusbar_GetHeightForSize(640, 400);
+		SBarHeight = Statusbar_GetHeightForSize(320, 200);
 
 		Resolution_GetLimits(&maxW, &maxH, &BitCount, ConfigureParams.Screen.bKeepResolutionST);
 		
 		/* Zoom if necessary, factors used for scaling mouse motions */
 		if (STRes == ST_LOW_RES &&
-		    2*Width <= maxW && 2*Height+SBarHeight <= maxH)
+		    1*Width <= maxW && 1*Height+SBarHeight <= maxH)
 		{
-			nZoom = 2;
-			Width *= 2;
-			Height *= 2;
-			nScreenZoomX = 2;
-			nScreenZoomY = 2;
-			bDoubleLowRes = true;
+			nZoom = 1;
+			Width *= 1;
+			Height *= 1;
+			nScreenZoomX = 1;
+			nScreenZoomY = 1;
+			bDoubleLowRes = false;
 		}
 		else if (STRes == ST_MEDIUM_RES)
 		{
@@ -638,7 +638,7 @@ static void Screen_SetResolution(void)
 			 * black for the TV mode).
 			 */
 			nScreenZoomX = 1;
-			nScreenZoomY = 2;
+			nScreenZoomY = 1;
  		}
 
 		/* Adjust width/height for overscan borders, if mono or VDI we have no overscan */
