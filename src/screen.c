2edebe17db5a16d3030709a92ba711dbb80b61c5
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-10-25 12:37:02 +0100

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -621,13 +621,13 @@ static void Screen_SetResolution(void)
 		
 		/* Zoom if necessary, factors used for scaling mouse motions */
 		if (STRes == ST_LOW_RES &&
-		    2*Width <= maxW && 2*Height+SBarHeight <= maxH)
+		    1*Width <= maxW && 1*Height+SBarHeight <= maxH)
 		{
-			nZoom = 2;
-			Width *= 2;
-			Height *= 2;
-			nScreenZoomX = 2;
-			nScreenZoomY = 2;
+			nZoom = 1;
+			Width *= 1;
+			Height *= 1;
+			nScreenZoomX = 1;
+			nScreenZoomY = 1;
 			bDoubleLowRes = false;
 		}
 		else if (STRes == ST_MEDIUM_RES)
