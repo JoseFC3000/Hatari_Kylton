1a7b75f9a8faaf9066b785d01d2df1cb227fe3ad
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-10-28 15:25:06 +0000

    Update libretro.c

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -28,8 +28,8 @@ cothread_t emuThread;
 int CROP_WIDTH;
 int CROP_HEIGHT;
 int VIRTUAL_WIDTH ;
-int retrow=1024; 
-int retroh=1024;
+int retrow=320; 
+int retroh=200;
 
 extern unsigned short int bmp[1024*1024];
 extern int STATUTON, SHOWKEY, SHIFTON, MOUSEMODE, PAS, SND;
