e38c70a34d523720ae6256744db1d9ad2ce4df68
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-10-28 04:34:33 +0000

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -623,12 +623,12 @@ static void Screen_SetResolution(void)
 		if (STRes == ST_LOW_RES &&
 		    2*Width <= maxW && 2*Height+SBarHeight <= maxH)
 		{
-			nZoom = 2;
+			nZoom = 1;
 			Width *= 2;
 			Height *= 2;
 			nScreenZoomX = 2;
 			nScreenZoomY = 2;
-			bDoubleLowRes = false;
+			bDoubleLowRes = true;
 		}
 		else if (STRes == ST_MEDIUM_RES)
 		{
